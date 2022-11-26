021ef595c7fd015e5d30519076c5ceaffad62aff
kavita <kavitamaji@yahoo.com>
2022-11-26 22:10:20 +0400

    callback and error handling

--- index.js
+++ index.js
@@ -3,17 +3,30 @@
     area:(x,y) => (x*y)
 };*/
 
+const rectangle = require('./rectangle');
 var rect = require('./rectangle');
 
 function solveRect(l, b) {
     console.log("solving for rectangle wih length as " + l + " and breadth " + b);
-    if (l <= 0 || b <= 0) {
+
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("error is " + err.message);
+        }
+        else {
+            console.log("length is " + l + " and breadth is " + b + " and area is " + rectangle.area() + " and perimeter is " + rectangle.perimeter());
+        }
+        
+    });
+    console.log("this will execute before the rect function even though its placed below it because Rect is delayed by settimeout");
+
+   /* if (l <= 0 || b <= 0) {
         console.log("Rect dimensions are incorrect - length is " + l + " and breadth is " + b);
     }
     else {
         console.log("Area of rectangle is" + rect.area(l, b));
         console.log("Perimeter of rectangle is" + rect.perimeter(l, b));
-    }
+    }*/
 }
 solveRect(3, 6);
 solveRect(2, 5);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,22 @@
-exports.perimeter = (x, y) => (2 * (x + y));
-exports.area = (x, y) => (x * y);
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        setTimeout(() =>
+            callback(new Error("Rect dimensions are incorrect - length is " + x + " and breadth is " + y),
+            null),// second arg null will be ignored when error is created
+            2000);
+    }
+    else {
+        setTimeout(() =>
+            callback(null,//I passed error as null
+                {
+                perimeter: () => (2 * (x + y)),
+                area: () => (x * y)
+            }),
+            2000);
+    }
+}
+
+
+
+/*exports.perimeter = (x, y) => (2 * (x + y));
+exports.area = (x, y) => (x * y);*/
